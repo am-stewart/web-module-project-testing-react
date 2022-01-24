74de0186a2bb022d4e0bf4be31569b061ae9a37a
Allison Stewart <amstewart@yahoo.com>
2022-01-24 14:45:39 -0500

    episode renders without errors

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -4,7 +4,17 @@ import { render, screen } from '@testing-library/react';
 import Episode from './../Episode';
 
 
-test("renders without error", () => {});
+test("renders without error", () => {
+  render(<Episode episode={{
+    id: 1,
+    img: 'https://i.ibb.co/2FsfXqM/stranger-things.png',
+    season: 1,
+    episode: 1,
+    name: 'episode name',
+    summary: 'summary',
+    runtime: 1
+  }} />);
+});
 
 test("renders the summary test passed as prop", ()=>{});
 
