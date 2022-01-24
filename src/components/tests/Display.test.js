a9413e7cb95fa2e3ccfa39d1890c1b56f55059ac
Allison Stewart <amstewart@yahoo.com>
2022-01-24 18:00:23 -0500

    all display tests passing

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -3,6 +3,9 @@ import '@testing-library/jest-dom/extend-expect';
 import { render, screen, waitFor} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
+import mockFetchShow from './../../api/fetchShow';
+jest.mock('./../../api/fetchShow');
+
 import Display from './../Display';
 
 const showData = {
@@ -31,7 +34,9 @@ test('renders without errors with no props', ()=>{
   render(<Display />)
 });
 
-test('renders Show component when the button is clicked ', async ()=>{
+test('renders Show component when the button is clicked ', async () => {
+  mockFetchShow.mockResolvedValueOnce(showData);
+
   //Arrange: display component 
   render(<Display />)
 
@@ -48,4 +53,31 @@ test('renders Show component when the button is clicked ', async ()=>{
   expect(showComponent).toBeInTheDocument();
 });
 
-test('renders show season options matching your data when the button is clicked', ()=>{});
+test('renders show season options matching your data when the button is clicked', async ()=>{
+  mockFetchShow.mockResolvedValueOnce(showData);
+
+  //Arrange: renders display with show data props
+  render(<Display show={showData}/>)
+
+  //Act
+  //1. find get data button 
+  const button = screen.getByRole('button');
+  //2. click button 
+  userEvent.click(button);
+
+  //Assert
+  //5. find number of dropdown options
+  const seasons = await (screen.findAllByTestId('season-option'))
+  //6. expect number of dropdown options === number of seasons(3)
+  expect(seasons).toHaveLength(3);
+});
+
+test('displayFunc is called when fetch button is pressed', () => {
+  mockFetchShow.mockResolvedValueOnce(showData);
+
+  render(<Display />)
+  const button = screen.getByRole('button');
+  userEvent.click(button);
+
+  expect(mockFetchShow).toHaveBeenCalled();
+});
